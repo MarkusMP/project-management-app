93035d57425a2aecf8f677dd16cb6176865df333
Markus Persson <markusperssonbiz@gmail.com>
2022-06-22 15:23:48 +0200

    Added Docker dev and prod environment with Nginx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
 }
 bootstrap();
