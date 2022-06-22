c24de6bd94238735ae5d47c75b191d3eb05f264a
Markus Persson <markusperssonbiz@gmail.com>
2022-06-22 21:17:22 +0200

    Added database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import {
+  DatabaseService,
+  databaseService,
+} from './modules/database/database.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(databaseService.getTypeOrmConfig()),
+    ProjectsModule,
+    ClientsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [DatabaseService],
 })
 export class AppModule {}
